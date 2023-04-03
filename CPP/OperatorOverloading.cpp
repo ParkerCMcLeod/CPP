dd9e78986619d1c88bcd03a57a913bfd5886a36a
Parker McLeod <ParkerCMcLeod@gmail.com>
2023-04-03 03:10:49 -0400

    more stuffs

---
+++ CPP/OperatorOverloading.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+class Vector
+{
+public:
+	int m_X;
+	int m_Y;
+
+	Vector(int x, int y)
+		: m_X(x), m_Y(y) {}
+
+	Vector* operator+(Vector vector)
+	{
+		this->m_X += vector.m_X;
+		this->m_Y += vector.m_Y;
+		return this;
+	}
+
+};
+
+
+// operator overloading!!!
+int main()
+{
+
+	Vector v0(10, 10);
+	Vector v1(5, 5);
+	Vector* v2;
+
+	v2 = v0 + v1;
+
+	std::cout << v2->m_X << ',' << v2->m_Y;
+	
+
+}
